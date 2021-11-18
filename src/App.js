8d44eada310d2a0cbb5d8e427d784748054250e9
Ren Tran <zaraku2@live.com>
2021-11-18 00:50:07 -0800

    added functional date input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import './App.css';
 import uniqid from "uniqid";
 import { Button, TextField } from '@mui/material';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { DatePicker } from '@mui/lab';
 
 class App extends Component {
   constructor(){
@@ -15,30 +18,32 @@ class App extends Component {
         location: '',
         description: '',
       },
-      workExp: [
+      workForms: [
         {
           id: uniqid(),
           position: '',
           company: '',
-          from: '',
-          to: '',
+          from: new Date(),
+          to: new Date(),
           description: '',
         },
       ],
-      education: [
+      eduForms: [
         {
           id: uniqid(),
           title: '',
           organization: '',
-          from: '',
-          to: ''
+          from: new Date(),
+          to: new Date(),
         }
       ]
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.addWorkForm = this.addWorkForm.bind(this);
+    //this.setWorkDate = this.setWorkDate.bind(this);
     this.addEducationForm = this.addEducationForm.bind(this);
+    this.setEducationDate = this.setEducationDate.bind(this);
   }
 
   handleSubmit(e){
@@ -54,9 +59,25 @@ class App extends Component {
       to: '',
       description: '',
     };
-    const forms = [...this.state.workExp, newForm];
+    const forms = [...this.state.workForms, newForm];
     this.setState({
-      workExp: forms,
+      workForms: forms,
+    });
+  }
+
+  setWorkDate(obj,type,newDate) {
+    const {workForms} = this.state;
+    const target = workForms.filter(form => form.id === obj.id)[0];
+    target[type] = newDate;
+    const newForms = workForms.map(form => {
+      if (form.id === target.id) {
+        return target;
+      } else {
+        return form;
+      }
+    });
+    this.setState({
+      workForms: newForms,
     });
   }
 
@@ -68,30 +89,84 @@ class App extends Component {
       from: '',
       to: ''
     };
-    const forms = [...this.state.education, newForm];
+    const forms = [...this.state.eduForms, newForm];
     this.setState({
-      education: forms,
+      eduForms: forms,
+    });
+  }
+
+  setEducationDate(obj, type, newDate) {
+    const {eduForms} = this.state;
+    const target = eduForms.filter(form => form.id === obj.id)[0];
+    target[type] = newDate;
+    const newForms = eduForms.map(form => {
+      if (form.id === target.id) {
+        return target;
+      } else {
+        return form;
+      }
+    });
+    this.setState({
+      eduForms: newForms,
     });
   }
 
   render() {
-    const workForms = this.state.workExp.map(work => (
-      <div id={work.id} className="work-form">
+    const workForms = this.state.workForms.map(form => (
+      <div id={form.id} className="work-form">
+
         <TextField label="Position" variant="outlined" margin="dense" size="small"/>
+
         <TextField label="Company" variant="outlined" margin="dense" size="small"/>
-        <TextField label="From" variant="outlined" margin="dense" size="small"/>
-        <TextField label="To" variant="outlined" margin="dense" size="small"/>
-        <TextField label="Description" variant="outlined" margin="dense" size="small"/>
+
+        <DatePicker
+          label="From"
+          views={['year', 'month']}
+          value={form.from || new Date()}
+          onChange={(newDate) => this.setWorkDate(form,"from",newDate)}
+          renderInput={(params) => <TextField {...params} />}
+        />
+
+        <DatePicker
+          label="To"
+          size="small"
+          views={['year', 'month']}
+          minDate={form.from}
+          value={form.to || new Date()}
+          onChange={(newDate) => this.setWorkDate(form,"to",newDate)}
+          renderInput={(params) => <TextField {...params} />}
+        />
+
+        <TextField label="Description" variant="outlined" margin="dense" size="small" multiline="true" minRows="2" maxRows="2"/>
+        
         <Button variant="outlined" color="error">Delete</Button>
       </div>
     ));
 
-    const eduForms = this.state.education.map(edu => (
-      <div id={edu.id} className="education-form">
+    const eduForms = this.state.eduForms.map(form => (
+      <div id={form.id} className="education-form">
+
         <TextField label="Title" variant="outlined" margin="dense" size="small"/>
+
         <TextField label="Organization" variant="outlined" margin="dense" size="small"/>
-        <TextField label="From" variant="outlined" margin="dense" size="small"/>
-        <TextField label="To" variant="outlined" margin="dense" size="small"/>
+
+        <DatePicker
+          label="From"
+          views={['year', 'month']}
+          value={form.from || new Date()}
+          onChange={(newDate) => this.setEducationDate(form,"from",newDate)}
+          renderInput={(params) => <TextField {...params} />}
+        />
+
+        <DatePicker
+          label="To"
+          views={['year', 'month']}
+          minDate={form.from}
+          value={form.to || new Date()}
+          onChange={(newDate) => this.setEducationDate(form,"to",newDate)}
+          renderInput={(params) => <TextField {...params} />}
+        />
+
         <Button variant="outlined" color="error">Delete</Button>
       </div>
     ));
@@ -105,20 +180,21 @@ class App extends Component {
           <TextField id="emailInput" label="Email" variant="outlined" margin="dense" size="small"/>
           <TextField id="phoneInput" label="Phone" variant="outlined" margin="dense" size="small"/>
           <TextField id="locationInput" label="Location" variant="outlined" margin="dense" size="small"/>
-          <TextField id="personalDescInput" label="Description" variant="outlined" margin="dense" size="small"/>
+          <TextField id="personalDescInput" label="Description" variant="outlined" margin="dense" size="small" multiline="true" minRows="2" maxRows="2"/>
         </div>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <div className="work-form-wrapper">
+            <h2>Work Experience</h2>
+            {workForms}
+            <Button variant="contained" onClick={this.addWorkForm}>Add</Button>
+          </div>
 
-        <div className="work-form-wrapper">
-          <h2>Work Experience</h2>
-          {workForms}
-          <Button variant="contained" onClick={this.addWorkForm}>Add</Button>
-        </div>
-
-        <div className="edu-form-wrapper">
-          <h2>Education</h2>
-          {eduForms}
-          <Button variant="contained" onClick={this.addEducationForm}>Add</Button>
-        </div>
+          <div className="edu-form-wrapper">
+            <h2>Education</h2>
+            {eduForms}
+            <Button variant="contained" onClick={this.addEducationForm}>Add</Button>
+          </div>
+        </LocalizationProvider>
       </div>
     )
   }
